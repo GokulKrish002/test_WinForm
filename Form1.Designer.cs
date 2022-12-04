720a79a6661b8f6c1cdf6841d4561f359a619b74
Gokul <itsgokul2002@gmail.com>
2022-12-04 10:36:23 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace test_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.btn_Submit2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "one",
+            "two",
+            "three",
+            "four"});
+            this.comboBox.Location = new System.Drawing.Point(409, 81);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(181, 24);
+            this.comboBox.TabIndex = 0;
+            this.comboBox.DropDown += new System.EventHandler(this.comboBox_DropDown);
+            this.comboBox.TextChanged += new System.EventHandler(this.comboBox_TextChanged);
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.Location = new System.Drawing.Point(351, 283);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(143, 58);
+            this.btn_Submit.TabIndex = 1;
+            this.btn_Submit.Text = "submit";
+            this.btn_Submit.UseVisualStyleBackColor = true;
+            // 
+            // btn_Submit2
+            // 
+            this.btn_Submit2.Location = new System.Drawing.Point(530, 283);
+            this.btn_Submit2.Name = "btn_Submit2";
+            this.btn_Submit2.Size = new System.Drawing.Size(143, 58);
+            this.btn_Submit2.TabIndex = 1;
+            this.btn_Submit2.Text = "submit";
+            this.btn_Submit2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(1173, 659);
+            this.Controls.Add(this.btn_Submit2);
+            this.Controls.Add(this.btn_Submit);
+            this.Controls.Add(this.comboBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Button btn_Submit;
+        private System.Windows.Forms.Button btn_Submit2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox_DropDown(object sender, EventArgs e)
+        {
+            /*btn_Submit.Enabled = true;
+            btn_Submit2.Enabled = true;*/
+        }
+        private void comboBox_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox.Text == "one")
+            {
+                btn_Submit.Enabled = false;
+            }
+            else if (comboBox.Text == "two")
+            {
+                btn_Submit2.Enabled = false;
+            }
+            else if (comboBox.Text == "three")
+            {
+                btn_Submit.Enabled = false;
+                btn_Submit2.Enabled = false;
+            }
+            else if (comboBox.Text == "four")
+            {
+                btn_Submit.Enabled = true;
+                btn_Submit2.Enabled = true;
+            }
+        }
+    }
+}
